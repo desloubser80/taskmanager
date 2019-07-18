41970870e5cd35d8ec1595920d474a4d32f1bc8c
des <desloubser2@gmail.com>
2019-07-18 08:58:58 +0200

    commit to include jwt

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req,res,next)=> {
+    if(req.method){
+        return res.status(503).send("CURRENTLY UNDER CONSTRUCTION. PLEASE FFFTTTTSSSEKKK")
+    }
+    next();
+});
+
+app.use((req,res,next)=> {
+    if(req.method==="GET"){
+        return res.send("IXNAY")
+    }
+    next();
+});
+
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -17,13 +32,13 @@ app.listen(port, ()=>{
     console.log("Server is running on port "+ port);
 })
 
-const jwt = require('jsonwebtoken')
-const myFunction = async () => {
-    const token = jwt.sign({ _id:'abc123' }, 'thisismynewcourse', {expiresIn : '7 seconds'});
-    console.log(token);
+// const jwt = require('jsonwebtoken')
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id:'abc123' }, 'thisismynewcourse', {expiresIn : '7 seconds'});
+//     console.log(token);
     
-    const data = jwt.verify(token,'thisismynewcourse' );
-    console.log(data)
-}
+//     const data = jwt.verify(token,'thisismynewcourse' );
+//     console.log(data)
+// }
 
-myFunction()
+// myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,26 +2,31 @@ const express = require('express')
 const router = new express.Router();
 const User = require('../models/user');
 
+//create new user
 router.post('/users',async (req,res)=>{
     const user = new User(req.body);
     try{
         await user.save()
-        res.status(201).send()
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token})
     }catch(e){
         res.status(400).send()
     }
 })
 
+//log in user
 router.post('/users/login', async(req,res) => {
     try{
         const user = await User.findByCredentials(req.body.email,req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken();
+        res.send({user, token})
     }
-    catch{
+    catch(e){
         res.status(400).send();
     }
 })
 
+//get all users
 router.get('/users', async (req,res)=>{
     try{
         const users  = await User.find({})
@@ -31,6 +36,7 @@ router.get('/users', async (req,res)=>{
     }
 })
 
+//get a specific user by id
 router.get('/users/:id', async (req,res)=>{
     const id = req.params.id
     try{
@@ -45,6 +51,7 @@ router.get('/users/:id', async (req,res)=>{
     }
 })
 
+//update user
 router.patch('/users/:id', async (req,res)=> {
     var updates = Object.keys(req.body);
     var allowedUpdates = ['name','age','email','password']
@@ -75,6 +82,7 @@ router.patch('/users/:id', async (req,res)=> {
     }
 })
 
+//delete user
 router.delete('/users/:id', async (req,res) => {
     try{
         var user = await User.findByIdAndDelete(req.params.id);
